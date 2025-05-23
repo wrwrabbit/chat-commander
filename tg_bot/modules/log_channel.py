b782ddb6e9a8c1de9bed247c59e68e5b13460596
204248929 <204248929+204248929@users.noreply.github.com>
2025-05-24 00:03:09 +0300

    Refactor log module

--- tg_bot/modules/log_channel.py
+++ tg_bot/modules/log_channel.py
@@ -1,25 +1,27 @@
 from functools import wraps
-from typing import Optional
+from typing import Optional, Awaitable, Callable
 
+from tg_bot.modules.helper_funcs.handlers import create_handler
 from tg_bot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
 if is_module_loaded(FILENAME):
-    from telegram import Bot, Update, ParseMode, Message, Chat
-    from telegram.error import BadRequest, Unauthorized
-    from telegram.ext import CommandHandler, run_async
-    from telegram.utils.helpers import escape_markdown
+    from telegram import Update, Message, Chat, Bot, MessageOriginChat, MessageOriginChannel
+    from telegram.constants import ParseMode, ChatType
+    from telegram.error import BadRequest, Forbidden
+    from telegram.ext import ContextTypes, filters
+    from telegram.helpers import escape_markdown
 
-    from tg_bot import dispatcher, LOGGER
+    from tg_bot import application, LOGGER
     from tg_bot.modules.helper_funcs.chat_status import user_admin
     from tg_bot.modules.sql import log_channel_sql as sql
 
 
-    def loggable(func):
+    def loggable(func: Callable[[Update, ContextTypes.DEFAULT_TYPE], Awaitable[Optional[str]]]):
         @wraps(func)
-        def log_action(bot: Bot, update: Update, *args, **kwargs):
-            result = func(bot, update, *args, **kwargs)
+        async def log_action(update: Update, context: ContextTypes.DEFAULT_TYPE):
+            result = await func(update, context)
             chat = update.effective_chat  # type: Optional[Chat]
             message = update.effective_message  # type: Optional[Message]
             if result:
@@ -29,7 +31,7 @@ if is_module_loaded(FILENAME):
                                                                                            message.message_id)
                 log_chat = sql.get_chat_log_channel(chat.id)
                 if log_chat:
-                    send_log(bot, log_chat, chat.id, result)
+                    await send_log(context.bot, log_chat, str(chat.id), result)
             elif result == "":
                 pass
             else:
@@ -40,71 +42,72 @@ if is_module_loaded(FILENAME):
         return log_action
 
 
-    def send_log(bot: Bot, log_chat_id: str, orig_chat_id: str, result: str):
+    async def send_log(bot: Bot, log_chat_id: str, orig_chat_id: str, result: str):
         try:
-            bot.send_message(log_chat_id, result, parse_mode=ParseMode.HTML)
+            await bot.send_message(log_chat_id, result, parse_mode=ParseMode.HTML)
         except BadRequest as excp:
             if excp.message == "Chat not found":
-                bot.send_message(orig_chat_id, "This log channel has been deleted - unsetting.")
+                await bot.send_message(orig_chat_id, "This log channel has been deleted - unsetting.")
                 sql.stop_chat_logging(orig_chat_id)
             else:
                 LOGGER.warning(excp.message)
                 LOGGER.warning(result)
                 LOGGER.exception("Could not parse")
 
-                bot.send_message(log_chat_id, result + "\n\nFormatting has been disabled due to an unexpected error.")
+                await bot.send_message(log_chat_id, result +
+                                       "\n\nFormatting has been disabled due to an unexpected error.")
 
 
     # @run_async
     @user_admin
-    def logging(bot: Bot, update: Update):
+    async def logging(update: Update, context: ContextTypes.DEFAULT_TYPE):
         message = update.effective_message  # type: Optional[Message]
         chat = update.effective_chat  # type: Optional[Chat]
 
         log_channel = sql.get_chat_log_channel(chat.id)
         if log_channel:
-            log_channel_info = bot.get_chat(log_channel)
-            message.reply_text(
-                "This group has all it's logs sent to: {} (`{}`)".format(escape_markdown(log_channel_info.title),
-                                                                         log_channel),
-                parse_mode=ParseMode.MARKDOWN)
+            log_channel_info = await context.bot.get_chat(log_channel)
+            await message.reply_text(
+                "This group has all it's logs sent to: {} \(`{}`\)".format(
+                    escape_markdown(log_channel_info.title, version=2), log_channel),
+                parse_mode=ParseMode.MARKDOWN_V2)
 
         else:
-            message.reply_text("No log channel has been set for this group!")
+            await message.reply_text("No log channel has been set for this group!")
 
 
     # @run_async
     @user_admin
-    def setlog(bot: Bot, update: Update):
+    async def setlog(update: Update, context: ContextTypes.DEFAULT_TYPE):
         message = update.effective_message  # type: Optional[Message]
         chat = update.effective_chat  # type: Optional[Chat]
-        if chat.type == chat.CHANNEL:
-            message.reply_text("Now, forward the /setlog to the group you want to tie this channel to!")
+        if chat.type == ChatType.CHANNEL:
+            await message.reply_text("Now, forward the /setlog to the group you want to tie this channel to!")
 
-        elif message.forward_from_chat:
-            sql.set_chat_log_channel(chat.id, message.forward_from_chat.id)
+        elif message.forward_origin and isinstance(message.forward_origin, MessageOriginChannel):
+            origin_chat = message.forward_origin.chat
+            if origin_chat and origin_chat.type == ChatType.CHANNEL:
+                sql.set_chat_log_channel(chat.id, origin_chat.id)
             try:
-                message.delete()
+                await message.delete()
             except BadRequest as excp:
-                if excp.message == "Message to delete not found":
-                    pass
-                else:
-                    LOGGER.exception("Error deleting message in log channel. Should work anyway though.")
+                LOGGER.exception("Error deleting message in log channel. Should work anyway though: %s", excp.message)
 
             try:
-                bot.send_message(message.forward_from_chat.id,
+                await context.bot.send_message(origin_chat.id,
                                  "This channel has been set as the log channel for {}.".format(
                                      chat.title or chat.first_name))
-            except Unauthorized as excp:
+            except Forbidden as excp:
                 if excp.message == "Forbidden: bot is not a member of the channel chat":
-                    bot.send_message(chat.id, "Successfully set log channel!")
+                    await context.bot.send_message(chat.id, "Successfully set log channel! "
+                                                            "bot is not a member of the channel chat")
                 else:
-                    LOGGER.exception("ERROR in setting the log channel.")
+                    LOGGER.exception("ERROR in setting the log channel: %s", excp.message)
 
-            bot.send_message(chat.id, "Successfully set log channel!")
+            await context.bot.send_message(chat.id, "Successfully set log channel!")
 
         else:
-            message.reply_text("The steps to set a log channel are:\n"
+            await message.reply_text("The steps to set a log channel are:\n"
                                " - add bot to the desired channel\n"
                                " - send /setlog to the channel\n"
                                " - forward the /setlog to the group\n")
@@ -112,17 +115,17 @@ if is_module_loaded(FILENAME):
 
     # @run_async
     @user_admin
-    def unsetlog(bot: Bot, update: Update):
+    async def unsetlog(update: Update, context: ContextTypes.DEFAULT_TYPE):
         message = update.effective_message  # type: Optional[Message]
         chat = update.effective_chat  # type: Optional[Chat]
 
         log_channel = sql.stop_chat_logging(chat.id)
         if log_channel:
-            bot.send_message(log_channel, "Channel has been unlinked from {}".format(chat.title))
-            message.reply_text("Log channel has been un-set.")
+            await context.bot.send_message(log_channel, "Channel has been unlinked from {}".format(chat.title))
+            await message.reply_text("Log channel has been un-set.")
 
         else:
-            message.reply_text("No log channel has been set yet!")
+            await message.reply_text("No log channel has been set yet!")
 
 
     def __stats__():
@@ -136,34 +139,36 @@ if is_module_loaded(FILENAME):
     def __chat_settings__(chat_id, user_id):
         log_channel = sql.get_chat_log_channel(chat_id)
         if log_channel:
-            log_channel_info = dispatcher.bot.get_chat(log_channel)
-            return "This group has all it's logs sent to: {} (`{}`)".format(escape_markdown(log_channel_info.title),
-                                                                            log_channel)
-        return "No log channel is set for this group!"
+            log_channel_info = application.bot.get_chat(log_channel)
+            return "This group has all it\'s logs sent to: {} \(`{}`\)".format(
+                escape_markdown(log_channel_info.title, version=2),
+                log_channel)
+        return "No log channel is set for this group\!"
 
 
     __help__ = """
-Настройка канала, в котором будут записываться определенные действия бота.
+Настройка канала, в котором будут записываться определенные действия бота\.
 Настройка выполняется так:
-- добавление бота на нужный канал (как админа!)
-- отправка /setlog в канал
-- пересылка /setlog в группу
+\- добавление бота на нужный канал \(как админа\!\)
+\- отправка /setlog в канал
+\- пересылка /setlog в группу
 
 *Только администратор:*
-- /logchannel: получить информацию о канале лога
-- /setlog: установить канал лога.
-- /unsetlog: отключить канал лога.
+\- /logchannel: получить информацию о канале лога
+\- /setlog: установить канал лога\.
+\- /unsetlog: отключить канал лога\.
 """
 
     __mod_name__ = "Канал лога"
 
-    LOG_HANDLER = CommandHandler("logchannel", logging)
-    SET_LOG_HANDLER = CommandHandler("setlog", setlog)
-    UNSET_LOG_HANDLER = CommandHandler("unsetlog", unsetlog)
+    LOG_HANDLER = create_handler("logchannel", logging)
+    SET_LOG_HANDLER = create_handler("setlog", setlog,
+                             filters=filters.ChatType.CHANNEL | filters.ChatType.GROUP | filters.ChatType.SUPERGROUP)
+    UNSET_LOG_HANDLER = create_handler("unsetlog", unsetlog)
 
-    dispatcher.add_handler(LOG_HANDLER)
-    dispatcher.add_handler(SET_LOG_HANDLER)
-    dispatcher.add_handler(UNSET_LOG_HANDLER)
+    application.add_handler(LOG_HANDLER)
+    application.add_handler(SET_LOG_HANDLER)
+    application.add_handler(UNSET_LOG_HANDLER)
 
 else:
     # run anyway if module not loaded
